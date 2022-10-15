cd8a4f64d58aa9fec80f014da913f834cca2ebca
mehedihasrifat <ebd.rifat101@gmail.com>
2022-10-15 08:44:09 +0600

    Solutions of 101 C Programming Problems

---
+++ 8 Start With String/P73LetterCount.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[1000];
+    int i;
+    int arr[123];
+
+    scanf("%s", str);
+
+    for (i = 0; i < 123; i++)
+    {
+        arr[i] = 0;
+    }
+
+    for (i = 0; i < strlen(str); i++)
+    {
+        arr[str[i]]++;
+    }
+
+    for (i = 0; i < 123; i++)
+    {
+        if (arr[i] != 0)
+        {
+            printf("%c(%d)", i, arr[i]);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
